c5468e5c4083e9d7895a4763b8692c7e5b67a355
wala481652 <wala481652@gmail.com>
2019-10-29 18:30:59 +0800

    新增2個步進馬達測試程式

---
+++ Stepper_motor_test/Stepper_motor_test.ino
@@ -0,0 +1,24 @@
+#include <Stepper.h>
+
+Stepper stepperL(2048, 8, 10, 9, 11);
+Stepper stepperR(2048, 4, 6, 5, 7);
+
+void setup() {
+  stepperL.setSpeed(15);
+  stepperR.setSpeed(15);
+}
+
+void loop() {
+  stepperL.step(2048);
+  stepperR.step(2048);
+  delay(500);
+  stepperL.step(-2048);
+  stepperR.step(-2048);
+  delay(500);
+  stepperL.step(2048);
+  stepperR.step(-2048);
+  delay(500);
+  stepperL.step(-2048);
+  stepperR.step(2048);
+  delay(500);
+}
